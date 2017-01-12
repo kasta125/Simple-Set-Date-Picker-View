4477abac855b1b847bece8843141a028cb3327d7
koryasta <student@koryasta.local>
2017-01-12 10:15:55 -0600

    Initial Commit

---
+++ Simple Picker View Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Picker View Calculator
+//
+//  Created by koryasta on 1/10/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+}
